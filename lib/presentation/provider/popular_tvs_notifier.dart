59bf53f78a0dcedf2d596d3795f99786a44090ab
Wildan Gunawan <35158077+wildangunawan@users.noreply.github.com>
2021-11-26 12:07:04 +0800

    Add provider for TV

---
+++ lib/presentation/provider/popular_tvs_notifier.dart
@@ -0,0 +1,39 @@
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/domain/usecases/tv/get_popular_tvs.dart';
+import 'package:flutter/foundation.dart';
+
+class PopularTVsNotifier extends ChangeNotifier {
+  final GetPopularTVs getPopularTVs;
+
+  PopularTVsNotifier(this.getPopularTVs);
+
+  RequestState _state = RequestState.Empty;
+  RequestState get state => _state;
+
+  List<TV> _tvs = [];
+  List<TV> get tvs => _tvs;
+
+  String _message = '';
+  String get message => _message;
+
+  Future<void> fetchPopularTVs() async {
+    _state = RequestState.Loading;
+    notifyListeners();
+
+    final result = await getPopularTVs.execute();
+
+    result.fold(
+      (failure) {
+        _message = failure.message;
+        _state = RequestState.Error;
+        notifyListeners();
+      },
+      (tvsData) {
+        _tvs = tvsData;
+        _state = RequestState.Loaded;
+        notifyListeners();
+      },
+    );
+  }
+}
