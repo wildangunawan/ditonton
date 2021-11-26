12c3e2edd44dd606ec1c239e3d780a653a4ce8cf
Wildan Gunawan <35158077+wildangunawan@users.noreply.github.com>
2021-11-26 12:06:57 +0800

    Rename class

--- lib/domain/usecases/tv/get_watchlist_tvs.dart
+++ lib/domain/usecases/tv/get_watchlist_tvs.dart
@@ -3,10 +3,10 @@ import 'package:ditonton/domain/entities/tv.dart';
 import 'package:ditonton/domain/repositories/tv_repository.dart';
 import 'package:ditonton/common/failure.dart';
 
-class GetWatchlistMovies {
+class GetWatchlistTVs {
   final TVRepository _repository;
 
-  GetWatchlistMovies(this._repository);
+  GetWatchlistTVs(this._repository);
 
   Future<Either<Failure, List<TV>>> execute() {
     return _repository.getWatchlistTVs();
