1bcd9efdc8a0ddeea1245369a7997dd6a8ad57e0
Wildan Gunawan <35158077+wildangunawan@users.noreply.github.com>
2021-11-26 10:59:42 +0800

    Add TV repository and usecases

---
+++ lib/domain/repositories/tv_repository.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/domain/entities/tv_detail.dart';
+import 'package:ditonton/common/failure.dart';
+
+abstract class TVRepository {
+  Future<Either<Failure, List<TV>>> getNowPlayingTVs();
+  Future<Either<Failure, List<TV>>> getPopularTVs();
+  Future<Either<Failure, List<TV>>> getTopRatedTVs();
+  Future<Either<Failure, TVDetail>> getTVDetail(int id);
+  Future<Either<Failure, List<TV>>> getTVRecommendations(int id);
+  Future<Either<Failure, List<TV>>> searchTVs(String query);
+  Future<Either<Failure, String>> saveWatchlist(TVDetail tv);
+  Future<Either<Failure, String>> removeWatchlist(TVDetail tv);
+  Future<bool> isAddedToWatchlist(int id);
+  Future<Either<Failure, List<TV>>> getWatchlistTVs();
+}
