873b00a5988de708909b00a838c66e027e3b2d21
Wildan Gunawan <35158077+wildangunawan@users.noreply.github.com>
2021-11-26 14:47:24 +0800

    Add dummy data for TVs

--- test/dummy_data/dummy_objects.dart
+++ test/dummy_data/dummy_objects.dart
@@ -1,7 +1,17 @@
 import 'package:ditonton/data/models/movie_table.dart';
+import 'package:ditonton/data/models/tv_table.dart';
 import 'package:ditonton/domain/entities/genre.dart';
 import 'package:ditonton/domain/entities/movie.dart';
 import 'package:ditonton/domain/entities/movie_detail.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/domain/entities/tv/created_by.dart';
+import 'package:ditonton/domain/entities/tv/episode_to_air.dart';
+import 'package:ditonton/domain/entities/tv/network.dart';
+import 'package:ditonton/domain/entities/tv/production_company.dart';
+import 'package:ditonton/domain/entities/tv/production_country.dart';
+import 'package:ditonton/domain/entities/tv/season.dart';
+import 'package:ditonton/domain/entities/tv/spoken_language.dart';
+import 'package:ditonton/domain/entities/tv_detail.dart';
 
 final testMovie = Movie(
   adult: false,
@@ -56,4 +66,76 @@ final testMovieMap = {
   'overview': 'overview',
   'posterPath': 'posterPath',
   'title': 'title',
+};
+
+final testTV = TV(
+    backdropPath: "/bSLURECWQ3xVhEpmFag6bznbAMx.jpg",
+    firstAirDate: DateTime.parse("2004-01-18"),
+    genreIds: [18],
+    id: 3475,
+    name: "The L Word",
+    originCountry: ["US", "CA"],
+    originalLanguage: "en",
+    originalName: "The L Word",
+    overview:
+        "A group of lesbian friends struggle with romance and careers in Los Angeles.",
+    popularity: 45.582,
+    posterPath: "/cRuye5mp69wfnsbfgJiBFQJQo1e.jpg",
+    voteAverage: 7.7,
+    voteCount: 598);
+
+final testTVList = [testTV];
+
+final testTVDetail = TVDetail(
+    backdropPath: "/bSLURECWQ3xVhEpmFag6bznbAMx.jpg",
+    createdBy: [CreatedBy(id: 4749, creditId: "525751bd760ee36aaa1cbea7", name: "Ilene Chaiken", gender: 1, profilePath: null)],
+    episodeRunTime: [50],
+    firstAirDate: DateTime.parse("2004-01-18"),
+    genres: [Genre(id: 18, name: "Drama")],
+    homepage: "http://www.sho.com/the-l-word",
+    id: 3475,
+    inProduction: false,
+    languages: ["en"],
+    lastAirDate: DateTime.parse("2009-03-08"),
+    lastEpisodeToAir: EpisodeToAir(airDate: DateTime.parse("2009-03-08"), episodeNumber: 8, id: 250819, name: "Last Word", overview: "In the final episode of the series, what starts out as a celebration of friendship quickly ends in a web of betrayal and deceit; the girls find themselves in the slammer with Sergeant Duffy; and the investigation into ? ? death begins…", productionCode: "", seasonNumber: 6, stillPath: "/itBBKWi7CAsm7m7hJqe1VmNvYDk.jpg", voteAverage: 5, voteCount: 1),
+    name: "The L Word",
+    nextEpisodeToAir: null,
+    networks: [Network(name: "Showtime", id: 67, logoPath: "/Allse9kbjiP6ExaQrnSpIhkurEi.png", originCountry: "US")],
+    numberOfEpisodes: 71,
+    numberOfSeasons: 6,
+    originCountry: ["CA", "US"],
+    originalLanguage: "en",
+    originalName: "The L Word",
+    overview: "A group of lesbian friends struggle with romance and careers in Los Angeles.",
+    popularity: 45.582,
+    posterPath: "/cRuye5mp69wfnsbfgJiBFQJQo1e.jpg",
+    productionCompanies: [ProductionCompany(id: 81151, logoPath: null, name: "Coast Mountain Films Studios", originCountry: "")],
+    productionCountries: [ProductionCountry(iso31661: "US", name: "United States of America")],
+    seasons: [Season(id: 10609, name: "Specials", overview: "", airDate: null, episodeCount: 4, posterPath: null, seasonNumber: 0)],
+    spokenLanguages: [SpokenLanguage(englishName: "English", iso6391: "en", name: "English")],
+    status: "Canceled",
+    tagline: "",
+    type: "Scripted",
+    voteAverage: 7.7,
+    voteCount: 598);
+
+final testWatchlistTV = TV.watchlist(
+  id: 1,
+  name: 'title',
+  posterPath: 'posterPath',
+  overview: 'overview',
+);
+
+final testTVTable = TVTable(
+  id: 1,
+  name: 'title',
+  posterPath: 'posterPath',
+  overview: 'overview',
+);
+
+final testTVMap = {
+  'id': 1,
+  'overview': 'overview',
+  'posterPath': 'posterPath',
+  'name': 'title',
 };
