2f44e76e927cdd16f832471bfeeaccf6bba99199
Wildan Gunawan <35158077+wildangunawan@users.noreply.github.com>
2021-11-26 20:09:30 +0800

    Change BASE_URL (typo)

--- lib/data/datasources/tv_remote_data_source.dart
+++ lib/data/datasources/tv_remote_data_source.dart
@@ -17,7 +17,7 @@ abstract class TVRemoteDataSource {
 
 class TVRemoteDataSourceImpl implements TVRemoteDataSource {
   static const API_KEY = 'api_key=2174d146bb9c0eab47529b2e77d6b526';
-  static const BASE_URL = 'https://api.theTVdb.org/3';
+  static const BASE_URL = 'https://api.themoviedb.org/3';
 
   final http.Client client;
 
