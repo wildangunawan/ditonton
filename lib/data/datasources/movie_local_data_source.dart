9bc5f7659c30916471068d9a5ecb947a2133c65d
Wildan Gunawan <35158077+wildangunawan@users.noreply.github.com>
2021-11-26 11:23:31 +0800

    Rename movie's database helper

--- lib/data/datasources/movie_local_data_source.dart
+++ lib/data/datasources/movie_local_data_source.dart
@@ -1,5 +1,5 @@
 import 'package:ditonton/common/exception.dart';
-import 'package:ditonton/data/datasources/db/database_helper.dart';
+import 'package:ditonton/data/datasources/db/movie_database_helper.dart';
 import 'package:ditonton/data/models/movie_table.dart';
 
 abstract class MovieLocalDataSource {
--- test/helpers/test_helper.mocks.dart
+++ test/helpers/test_helper.mocks.dart
@@ -8,7 +8,7 @@ import 'dart:typed_data' as _i19;
 
 import 'package:dartz/dartz.dart' as _i2;
 import 'package:ditonton/common/failure.dart' as _i8;
-import 'package:ditonton/data/datasources/db/database_helper.dart' as _i15;
+import 'package:ditonton/data/datasources/db/movie_database_helper.dart' as _i15;
 import 'package:ditonton/data/datasources/movie_local_data_source.dart' as _i13;
 import 'package:ditonton/data/datasources/movie_remote_data_source.dart'
     as _i11;
